910f1ee6d94f4aa7298f0a26a1100f4aa613bcdf
rubitopato <connorrgb@gmail.com>
2023-05-12 22:47:22 +0200

    ole

--- procedimientos/procedure_crear_oferta.sql
+++ procedimientos/procedure_crear_oferta.sql
@@ -5,12 +5,14 @@ BEGIN
 DECLARE done BOOLEAN;
 DECLARE valido BOOLEAN;
 DECLARE consulta VARCHAR(50);
+DECLARE estado_localidad VARCHAR(50);
 DECLARE recinto VARCHAR(100);
 DECLARE cursito CURSOR FOR (SELECT nombre_recinto_localidad FROM Localidad WHERE localizacion_localidad=localizacionLocalidad AND nombre_grada_localidad=nombreGrada);
 DECLARE CONTINUE HANDLER FOR NOT FOUND SET done=TRUE;
 SET valido = FALSE;
 SET done = FALSE;
 SELECT nombre_espectaculo_evento INTO consulta from Evento where nombre_recinto_evento = nombreRecinto and nombre_espectaculo_evento=nombreEspectaculo and fecha_evento=fechaEvento;
+SELECT estado_localidad INTO estado_localidad from Localidad where nombre_recinto_localidad=nombreRecinto and nombre_grada_localidad and localizacion_localidad=localizacionLocalidad;
 SELECT(consulta);
 OPEN cursito;
 
@@ -22,8 +24,10 @@ LEAVE loop1;
 ELSE 
 IF recinto=nombreRecinto THEN/*hace una operacion en concreto sobre cada tupla del cursor*/
 IF consulta IS NOT NULL THEN
+IF estado_localidad = 'disponible' THEN
 SET valido = TRUE;
 LEAVE loop1;
+END IF;
 END IF;
 END IF;
 END IF;

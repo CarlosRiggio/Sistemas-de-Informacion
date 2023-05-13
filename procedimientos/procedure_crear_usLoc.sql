d8a7280aca953be4b55dd843c8c02cc7508eadf4
Ruben Gonzalez Braña <connorrgb@gmail.com>
2023-05-13 12:33:20 +0200

    Create procedure_crear_usLoc.sql

---
+++ procedimientos/procedure_crear_usLoc.sql
@@ -0,0 +1,19 @@
+USE Taquilla;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS crear_usLoc//
+
+CREATE PROCEDURE crear_usLoc(IN localizacion_localidad_in VARCHAR(50), IN nombre_grada_in VARCHAR(50), IN nombre_recinto_in VARCHAR(50), IN tipo_usuario ENUM('jubilado','parado','adulto','infantil'))
+BEGIN
+
+DECLARE consulta VARCHAR(50);
+
+SELECT localizacion_localidad INTO consulta FROM Localidad WHERE nombre_recinto_localidad=nombre_recinto_in AND localizacion_localidad=localizacion_localidad_in AND nombre_grada_localidad=nombre_grada_in;
+
+IF localizacion_localidad = localizacion_localidad_in THEN
+INSERT INTO UsLoc VALUES (tipo_usuario,localizacion_localidad_in,nombre_grada_in,ombre_recinto_in);
+
+END//
+
+DELIMITER ;

e09c8aaae1e81daca162f58f234b7263e57fa04a
CarlosRiggio <criggio02@gmail.com>
2023-05-13 15:37:29 +0200

    LOCALIDAD Y GRADAS CAMBIADAS CON AFOROS

--- procedimientos/procedure_crear_gradas.sql
+++ procedimientos/procedure_crear_gradas.sql
@@ -1,3 +1,53 @@
+-- USE Taquilla;
+
+-- DELIMITER //
+
+-- DROP PROCEDURE IF EXISTS crear_gradas//
+
+-- CREATE PROCEDURE crear_gradas(
+--     IN nombre_grada_in VARCHAR(50), 
+--     IN nombre_recinto_grada_in VARCHAR(50), 
+--     IN num_localidades_reservar_grada_in INT, 
+--     IN precio_grada_in INT
+-- )
+-- BEGIN
+
+-- DECLARE localidades_recinto_proc INT;
+
+-- -- Obtener el número de localidades del recinto correspondiente
+-- SELECT localidades_recinto INTO localidades_recinto_proc 
+-- FROM Recinto WHERE nombre_recinto = nombre_recinto_grada_in;
+
+-- SELECT localidades_recinto_proc;
+-- SELECT num_localidades_reservar_grada_in;
+
+-- IF num_localidades_reservar_grada_in > localidades_recinto_proc THEN
+--     SIGNAL SQLSTATE '45001' 
+--         SET MESSAGE_TEXT = 'El número de localidades reservadas para la grada es mayor que el número de localidades disponibles en el recinto.';
+-- ELSE
+--     -- Crear la tabla si no existe y hacer la inserción
+--     CREATE TABLE IF NOT EXISTS Grada (
+--         nombre_grada VARCHAR(50),
+--         nombre_recinto_grada VARCHAR(50),
+--         num_localidades_reservar_grada INT,
+--         precio_grada INT,
+--         FOREIGN KEY(nombre_recinto_grada) REFERENCES Recinto(nombre_recinto),
+--         PRIMARY KEY(nombre_grada, nombre_recinto_grada)
+--     );
+    
+--     INSERT INTO Grada(nombre_grada, nombre_recinto_grada, num_localidades_reservar_grada, precio_grada) 
+--     VALUES (nombre_grada_in, nombre_recinto_grada_in, num_localidades_reservar_grada_in, precio_grada_in);
+-- END IF;
+
+-- END//
+
+-- DELIMITER ;
+
+-- CALL crear_gradas('GRADA PRUEBA2', 'Bernabeu', 5, 100);
+
+-- -- SELECT localidades_recinto FROM Recinto WHERE nombre_recinto = 'Bernabeu';
+
+
 USE Taquilla;
 
 DELIMITER //
@@ -13,17 +63,19 @@ CREATE PROCEDURE crear_gradas(
 BEGIN
 
 DECLARE localidades_recinto_proc INT;
+DECLARE total_localidades_reservadas INT;
 
 -- Obtener el número de localidades del recinto correspondiente
 SELECT localidades_recinto INTO localidades_recinto_proc 
 FROM Recinto WHERE nombre_recinto = nombre_recinto_grada_in;
 
-SELECT localidades_recinto_proc;
-SELECT num_localidades_reservar_grada_in;
+-- Obtener el sumatorio de localidades reservadas para el recinto correspondiente
+SELECT IFNULL(SUM(num_localidades_reservar_grada), 0) INTO total_localidades_reservadas
+FROM Grada WHERE nombre_recinto_grada = nombre_recinto_grada_in;
 
-IF num_localidades_reservar_grada_in > localidades_recinto_proc THEN
+IF total_localidades_reservadas + num_localidades_reservar_grada_in > localidades_recinto_proc THEN
     SIGNAL SQLSTATE '45001' 
-        SET MESSAGE_TEXT = 'El número de localidades reservadas para la grada es mayor que el número de localidades disponibles en el recinto.';
+        SET MESSAGE_TEXT = 'No hay tanto aforo';
 ELSE
     -- Crear la tabla si no existe y hacer la inserción
     CREATE TABLE IF NOT EXISTS Grada (
@@ -43,6 +95,4 @@ END//
 
 DELIMITER ;
 
-CALL crear_gradas('GRADA NORTE MADRID', 'Bernabeu', 600, 100);
-CALL crear_gradas('GRADA NORTE MADRID', 'Bernabeu', 499, 100);
--- SELECT localidades_recinto FROM Recinto WHERE nombre_recinto = 'Bernabeu';
+CALL crear_gradas('GRADA CN3', 'CAMP NOU', 5, 20);
--- procedimientos/procedure_crear_localidad.sql
+++ procedimientos/procedure_crear_localidad.sql
@@ -14,19 +14,32 @@ CREATE PROCEDURE crear_localidad(
 BEGIN
     DECLARE recinto_existe INT DEFAULT 0;
     DECLARE grada_existe INT DEFAULT 0;
+    DECLARE num_tuplas INT DEFAULT 0;
+    DECLARE num_localidades_reservar INT DEFAULT 0;
     
     -- Comprobar si el recinto existe
     SELECT COUNT(*) INTO recinto_existe FROM Recinto WHERE nombre_recinto = p_nombre_recinto;
     
-    -- Comprobar si la grada existe
-    SELECT COUNT(*) INTO grada_existe FROM Grada WHERE nombre_grada = p_nombre_grada AND nombre_recinto_grada = p_nombre_recinto;
+    -- Comprobar si la grada existe y obtener el número de localidades a reservar
+    SELECT num_localidades_reservar_grada, COUNT(*) INTO num_localidades_reservar, grada_existe 
+    FROM Grada WHERE nombre_grada = p_nombre_grada AND nombre_recinto_grada = p_nombre_recinto
+    GROUP BY num_localidades_reservar_grada;
     
     IF recinto_existe > 0 AND grada_existe > 0 THEN
-        -- Insertar nueva localidad si tanto el recinto como la grada existen
-        INSERT INTO Localidad(localizacion_localidad, nombre_recinto_localidad, nombre_grada_localidad, precio_base_localidad, estado_localidad)
-        VALUES (p_localizacion, p_nombre_recinto, p_nombre_grada, p_precio_base, p_estado);
+        -- Obtener el número de tuplas con el mismo recinto y la misma grada
+        SELECT COUNT(*) INTO num_tuplas FROM Localidad WHERE nombre_recinto_localidad = p_nombre_recinto AND nombre_grada_localidad = p_nombre_grada;
         
-        SELECT "La localidad ha sido creada exitosamente." AS mensaje;
+        -- Comprobar si se pueden reservar más localidades en la grada
+        IF num_tuplas < num_localidades_reservar THEN
+            -- Insertar nueva localidad si tanto el recinto como la grada existen y se pueden reservar más localidades
+            INSERT INTO Localidad(localizacion_localidad, nombre_recinto_localidad, nombre_grada_localidad, precio_base_localidad, estado_localidad)
+            VALUES (p_localizacion, p_nombre_recinto, p_nombre_grada, p_precio_base, p_estado);
+        
+            -- Imprimir el número de tuplas con el mismo recinto y la misma grada
+            SELECT CONCAT("La localidad ha sido creada exitosamente. Hay ", num_tuplas+1, " localidades en la misma grada y el mismo recinto.") AS mensaje;
+        ELSE
+            SELECT "Error: No se pueden reservar más localidades en esta grada." AS mensaje;
+        END IF;
     ELSE
         SELECT "Error: El recinto o la grada no existen." AS mensaje;
     END IF;
@@ -34,3 +47,7 @@ END//
 
 DELIMITER ;
 
+CALL crear_localidad('2', 'Bernabeu', 'GRADA PRUEBA2', 23, 'disponible');
+CALL crear_localidad('3', 'Bernabeu', 'GRADA PRUEBA2', 23, 'disponible');
+CALL crear_localidad('4', 'Bernabeu', 'GRADA PRUEBA2', 23, 'disponible');
+CALL crear_localidad('5', 'Bernabeu', 'GRADA PRUEBA2', 23, 'disponible');

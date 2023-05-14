5459cff626c1f399bfdfca77f061de9dd9eb34d6
Ruben Gonzalez Braña <connorrgb@gmail.com>
2023-05-14 20:51:36 +0200

    Update procedimientos_totales.sql

--- procedimientos/procedimientos_totales.sql
+++ procedimientos/procedimientos_totales.sql
@@ -211,8 +211,7 @@ BEGIN
 END//
 
 
-
-DROP PROCEDURE IF EXISTS crearOferta//
+DROP PROCEDURE IF EXISTS crearOferta;
 CREATE PROCEDURE crearOferta(
     IN nombreEspectaculo CHAR(50),
     IN nombreRecinto CHAR(50),
@@ -227,22 +226,36 @@ BEGIN
     DECLARE consulta VARCHAR(50);
     DECLARE localidad VARCHAR(50);
     DECLARE recinto VARCHAR(100);
+    DECLARE consulta_usloc VARCHAR(50);
     DECLARE cursito CURSOR FOR (SELECT nombre_recinto_localidad FROM Localidad WHERE localizacion_localidad=localizacionLocalidad AND nombre_grada_localidad=nombreGrada);
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done=TRUE;
     
     SET valido = FALSE;
     SET done = FALSE;
     
+    CREATE TABLE IF NOT EXISTS Oferta(
+        nombre_espectaculo_oferta VARCHAR(50),
+        nombre_recinto_oferta VARCHAR(50),
+        tipo_usuario_oferta ENUM('jubilado', 'parado', 'adulto', 'infantil'),
+        localizacion_localidad_oferta VARCHAR(50),
+        nombre_grada_oferta VARCHAR(50),
+        fecha_evento_oferta DATETIME,
+        estado_localidad_oferta ENUM('pre-reservado', 'reservado', 'deteriorado', 'libre'),
+        FOREIGN KEY(tipo_usuario_oferta, localizacion_localidad_oferta, nombre_grada_oferta, nombre_recinto_oferta) 
+        REFERENCES UsLoc(tipo_usuario_usloc, localizacion_localidad_usloc, nombre_grada_usloc, nombre_recinto_usloc),
+        PRIMARY KEY(nombre_espectaculo_oferta, nombre_recinto_oferta, tipo_usuario_oferta, localizacion_localidad_oferta, nombre_grada_oferta)
+    );
+    
     SELECT nombre_espectaculo_evento INTO consulta FROM Evento WHERE nombre_recinto_evento = nombreRecinto AND nombre_espectaculo_evento = nombreEspectaculo AND fecha_evento = fechaEvento;
     SELECT estado_localidad INTO localidad FROM Localidad WHERE nombre_recinto_localidad = nombreRecinto AND nombre_grada_localidad = nombreGrada AND localizacion_localidad = localizacionLocalidad;
-    SELECT(consulta);
+    SELECT tipo_usuario_usloc INTO consulta_usLoc FROM UsLoc WHERE tipo_usuario_usloc = tipoUsuarioOfertado AND localizacion_localidad_usloc = localizacionLocalidad AND nombre_grada_usloc = nombreGrada AND nombre_recinto_usloc = nombreRecinto;
     
     OPEN cursito;
     
     loop1:LOOP
         FETCH cursito INTO recinto;
-        SELECT(recinto);
-        SELECT(localidad);
+        /*SELECT(recinto);
+        SELECT(localidad);*/
         
         IF done = TRUE THEN 
             LEAVE loop1;
@@ -250,12 +263,20 @@ BEGIN
             IF recinto = nombreRecinto THEN
                 IF consulta IS NOT NULL THEN
                     IF localidad = 'disponible' THEN
-                        SET valido = TRUE;
-                        LEAVE loop1;
+                        IF consulta_usLoc IS NOT NULL THEN
+                            SET valido = TRUE;
+                            LEAVE loop1;
+                        ELSE 
+                            SELECT 'No se creo la oferta' AS mensaje;
+                        END IF;
                     ELSE 
                         SELECT 'No se creo la oferta' AS mensaje;
                     END IF;
+                ELSE 
+                    SELECT 'No se creo la oferta' AS mensaje;
                 END IF;
+            ELSE 
+                SELECT 'No se creo la oferta' AS mensaje;
             END IF;
         END IF;
     END LOOP;

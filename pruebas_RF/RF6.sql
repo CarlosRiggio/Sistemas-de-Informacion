3889595c0c72741bc72b02dc4fa6ccd22910da43
Rubén Castro González <101869123+lilruwu@users.noreply.github.com>
2023-05-14 18:26:43 +0200

    Update RF6.sql

--- pruebas_RF/RF6.sql
+++ pruebas_RF/RF6.sql
@@ -9,4 +9,4 @@ CALL CrearEvento ("El Clasico", "Camp Nou", "2023-5-12 5:00:00", "Cerrado");
 
 --Intenta comprar una entrada de un evento cerrado
 
-CALL ComprarEntrada('97780491', 'jubilado', 'El Clasico', '2023-05-15 01:00:00', 'Suelo 99', 'Grada Norte', 'Camp Nou');
+CALL ComprarEntrada('97780491', 'jubilado', 'El Clasico', '2023-05-12 5:00:00', 'Suelo 99', 'Grada Norte', 'Camp Nou');
